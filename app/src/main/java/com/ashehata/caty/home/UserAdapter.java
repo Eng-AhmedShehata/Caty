2cd5d346cb2cc9bd6a28a4eb43cb816eeae6b42a
ahamdyshehata7@gmail.com <ahamdyshehata7@gmail.com>
2021-01-23 19:43:24 +0200

    my first commit

---
+++ app/src/main/java/com/ashehata/caty/home/UserAdapter.java
@@ -0,0 +1,59 @@
+package com.ashehata.caty.home;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.ashehata.caty.R;
+
+import java.util.List;
+
+public class UserAdapter extends RecyclerView.Adapter<UserViewHolder> {
+
+    private OnUserClicked onUserClicked;
+
+    //1. define interface
+    public interface OnUserClicked {
+        void onUserSingleClicked(User user);
+        void onUserLongClicked(User user);
+        //.....
+    }
+
+    //3.
+    public void registerView(OnUserClicked onUserClicked) {
+        this.onUserClicked = onUserClicked;
+    }
+
+    private List<User> users;
+    private Activity activity;
+
+    public UserAdapter(List<User> users, OnUserClicked onUserClicked) {
+        this.users = users;
+        this.onUserClicked = onUserClicked;
+        //this.activity = activity;
+    }
+
+    @NonNull
+    @Override
+    public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View myView = inflater.inflate(R.layout.root_user, parent, false);
+        return new UserViewHolder(myView, onUserClicked);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
+        User user = users.get(position);
+        holder.bindData(user);
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+}
---
+++ app/src/main/java/com/ashehata/caty/home/UserViewHolder.java
@@ -0,0 +1,56 @@
+package com.ashehata.caty.home;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.ashehata.caty.R;
+import com.bumptech.glide.Glide;
+
+public class UserViewHolder extends RecyclerView.ViewHolder {
+
+    private UserAdapter.OnUserClicked onUserClicked;
+    private TextView title;
+    private TextView des;
+    private ImageView image;
+    private LinearLayout parent;
+
+    public UserViewHolder(@NonNull View itemView, UserAdapter.OnUserClicked onUserClicked) {
+        super(itemView);
+        parent = itemView.findViewById(R.id.parent_linear);
+        title = itemView.findViewById(R.id.tv_title);
+        des = itemView.findViewById(R.id.tv_des);
+        image = itemView.findViewById(R.id.iv_image);
+        this.onUserClicked = onUserClicked;
+    }
+
+    public void bindData(User user) {
+        //...
+        title.setText(user.getTitle());
+        des.setText(user.getDescription());
+        // load my image
+        Glide.with(image.getContext())
+                .load(user.getImageUrl())
+                .centerCrop().into(image);
+
+        //...
+        parent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onUserClicked.onUserSingleClicked(user);
+            }
+        });
+
+        parent.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                onUserClicked.onUserLongClicked(user);
+                return true;
+            }
+        });
+    }
+}
